a81eb2065d62dc577799f3bee48fa78f831be1ab
GenkiKishikawa <gen-at86.battleship.930@docomo.ne.jp>
2024-07-17 09:02:15 +0000

    Add: メイン画面と学習時間計測のapi連携

--- react/src/components/Main.tsx
+++ react/src/components/Main.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import {
   Toolbar,
   List,
@@ -11,17 +12,29 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+import { getRecords, deleteRecord } from '../api/request';
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 
 const drawerWidth = 120;
 
-const timeRecords = [
-  { id: 1, duration: '1.5h', start: '2024/07/08 12:30', end: '2024/07/08 14:00' },
-  // More records...
-];
-
 export const Main = () => {
+  const [records, setRecords] = useState([]); // recordsの状態を初期化
+
+  useEffect(() => {
+    getRecords().then(res => {
+      setRecords(res.data); // APIから取得したデータでrecordsを更新
+      console.log(res.data);
+    }).catch(err => console.error('Failed to fetch records:', err));
+  }, []);
+
+  const handleDeleteRecord = (id: number) => {
+    deleteRecord(id).then(res => {
+      const updatedRecords = records.filter(record => record.id !== id);
+      setRecords(updatedRecords);
+    }).catch(err => console.error('Failed to delete record:', err));
+  };
+
   return (
     <Box sx={{ display: 'flex' }}>
       <Header />
@@ -29,19 +42,19 @@ export const Main = () => {
       <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}>
         <Toolbar />
         <List>
-          {timeRecords.map((record) => (
+          {records.map((record) => (
             <ListItem key={record.id} secondaryAction={
               <>
                 <IconButton edge="end" aria-label="edit">
                   <EditIcon />
                 </IconButton>
-                <IconButton edge="end" aria-label="delete">
+                <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteRecord(record.id)} >
                   <DeleteIcon />
                 </IconButton>
               </>
             }>
               <AccessTimeIcon style={{ marginRight: '10px' }} />
-              <ListItemText primary={`${record.duration} ${record.start} ~ ${record.end}`} />
+              <ListItemText primary={`${record.studyTime} ${record.startYear}-${record.startMonth}-${record.startDay} ${record.startTime} ~ ${record.endYear}-${record.endMonth}-${record.endDay} ${record.endTime}`} />
             </ListItem>
           ))}
         </List>
--- react/src/components/Stopwatch.tsx
+++ react/src/components/Stopwatch.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
 import { useStopwatch } from "react-timer-hook";
+import { useNavigate } from "react-router-dom";
+
+import { postRecord } from "../api/request";
 
 export const Stopwatch = () => {
   const {
@@ -11,21 +15,54 @@ export const Stopwatch = () => {
     start,
     pause,
     reset,
-  } = useStopwatch({ autoStart: true });
+  } = useStopwatch();
+
+  const [startTimeValue, setStartTimeValue] = useState("");
+
+  const handleStart = () => {
+    if (!startTimeValue) {
+      setStartTimeValue(new Date().toString());
+    }
+    start();
+  }
+
+  const handleReset = () => {
+    reset();
+    setStartTimeValue("");
+  }
+
+  const navigate = useNavigate();
 
   const handleSave = async () => {
+    const params = {
+      studyTime: `${hours}:${minutes}:${seconds}`,
+      startYear: new Date(startTimeValue).getFullYear(),
+      startMonth: new Date(startTimeValue).getMonth() + 1,
+      startDay: new Date(startTimeValue).getDate(),
+      startTime: new Date(startTimeValue).toLocaleTimeString(),
+      endYear: new Date().getFullYear(),
+      endMonth: new Date().getMonth() + 1,
+      endDay: new Date().getDate(),
+      endTime: new Date().toLocaleTimeString(),
+    };
 
+    const res = await postRecord(params);
+
+    console.log(res);
+
+    navigate('/');
   }
 
   return (
     <div>
       <h1>Timer</h1>
+      <a>start: {startTimeValue}</a>
       <div style={{ fontSize: '100px' }}>
         <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
       </div>
-      <button onClick={start}>Start</button>
+      <button onClick={handleStart}>Start</button>
       <button onClick={pause}>Pause</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={handleReset}>Reset</button>
       <button onClick={handleSave}>Save</button>
     </div>
   )
